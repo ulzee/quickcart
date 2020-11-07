c8aa2c6f45e1152a6c36c3c6c1a1ef030380458d
Ulzee An <ul1994@gmail.com>
2020-11-07 08:22:29 -0800

    adding newegg checkout

---
+++ stores/newegg.js
@@ -0,0 +1,136 @@
+
+const { sec } = require('../utils');
+const utils = require('../utils');
+const { nav, any, inp, sel, paste, click } = require('../actions');
+
+const domain = 'https://www.newegg.com';
+
+const vendor = 'NEWEGG';
+const log = utils.taglog(vendor);
+global.log = log;
+
+module.exports = {
+	home: domain,
+	*prime(page, args) {
+		const {
+			account: { user, pass },
+		} = args;
+
+		if (args.nologin) return;
+
+		// give up on this IP if pages too slow
+		yield nav.bench(page, domain, waitFor='#SearchBox2020');
+		yield page.waitForTimeout(5 * sec);
+		yield page.$$eval('.nav-complex a.nav-complex-inner', ls => ls[0].click());
+		yield page.waitForTimeout(5 * sec);
+
+		yield page.waitForSelector('#labeled-input-signEmail');
+		yield page.waitForTimeout(sec);
+		yield page.type('#labeled-input-signEmail', user);
+		yield page.waitForTimeout(sec);
+		yield page.click('#signInSubmit');
+		yield page.waitForTimeout(30 * sec);
+
+		yield page.waitForSelector('.header2020-search-box');
+		yield page.waitForTimeout(5 * sec);
+
+		//empty cart
+		yield nav.go(page, 'https://secure.newegg.com/shop/cart');
+		yield page.waitForTimeout(5 * sec);
+
+		try {
+			yield click(page, 'button[data-target="#Popup_Remove_All"]');
+			yield click(page, '.modal-content .btn-primary');
+			yield page.waitForTimeout(5 * sec);
+		}
+		catch(e) {
+			log('Nothing to remove..');
+		}
+	},
+	*visit(page, url) {
+		yield nav.bench(page, url, waitFor='.price-current-label');
+	},
+	*standby(page, args) {
+		yield page.waitForSelector('.price-current-label');
+
+		while (true) {
+
+			try {
+				yield page.waitForSelector('#ProductBuy .btn-primary');
+				break;
+			}
+			catch (e) {
+				let oosText = yield page.$eval('#ProductBuy .btn-secondary', el => el ? el.textContent : null);
+
+				log('OOS: ' + oosText);
+
+				const waitTime = utils.eta();
+				log('Waiting: ' + waitTime.toFixed(2));
+				yield page.waitForTimeout(waitTime * sec);
+
+				yield nav.bench(page, args.url, waitFor='.price-current-label', retry=true);
+			}
+		}
+	},
+	*checkout(page, args) {
+		let {
+			url,
+			account: {
+				security,
+				number, month, year
+			},
+			logid,
+		} = args;
+
+		log(url);
+
+		yield click(page, '#ProductBuy .btn-primary');
+
+		// wait for confirmation prompt then go to cart
+		yield page.waitForSelector('.modal-content'); // TODO: wait for checkout to be enabled
+		yield nav.go(page, 'https://secure.newegg.com/shop/cart?Submit=view');
+		yield click(page, '.summary-actions .btn-primary');
+
+		// newegg asks for number retype sometimes
+		page.waitForSelector('.mask-cardnumber', { timeout: 5 * 60 * sec })
+		.then(() => {
+			return page.type('.mask-cardnumber', number);
+		})
+		.then(() => {
+			return click(page, '.modal-content .btn-primary');
+		})
+		.catch(console.log);
+
+
+		// In checkout window
+		// NOTE: shipping methods may need to be reselected
+		// yield click(page, '.checkout-step-action-done');
+		// yield click(page, '.checkout-step-action-done');
+		yield page.waitForSelector('.form-text.mask-cvv-4');
+		yield page.type('.form-text.mask-cvv-4', security);
+
+		if (args.debug == undefined || args.debug == false) {
+			while (true) {
+				const bgColor = yield page.$eval('#btnCreditCard', el => {
+					const button = document.querySelector('#btnCreditCard');
+					return getComputedStyle(button).backgroundColor;
+				});
+
+				if (bgColor == 'rgb(255, 163, 58)') {
+					break;
+				}
+				else {
+					yield page.waitForSelector(50);
+				}
+			}
+
+			yield click(page, '#btnCreditCard');
+		}
+
+		// checkout success
+		log('Done!');
+		yield page.waitForTimeout(10 * sec);
+		yield page.screenshot({path: `logs/ok_${logid}.png`});
+		yield page.waitForTimeout(10 * sec);
+	},
+}
