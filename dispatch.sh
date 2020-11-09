ba2e0510cc962260d37ccdca41a2bb116a11d7a0
Ulzee An <ul1994@gmail.com>
2020-11-09 12:01:14 -0800

    more settings for bot dispatch

--- dispatch.sh
+++ dispatch.sh
@@ -1,55 +1,22 @@
 
-num=$1
-debug="true"
+source ./urls.sh
 
-# FIXME: add bnh
 # stores="bestbuy target walmart newegg adorama kohls"
 stores="bestbuy target walmart"
 
-# ps5
-bestbuy="https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p"
-target="https://www.target.com/p/playstation-5-console/-/A-81114595"
-walmart="https://www.walmart.com/ip/PlayStation-5-Console/363472942"
-newegg="https://www.newegg.com/p/N82E16868110292?Description=playstation%205&cm_re=playstation_5-_-68-110-292-_-Product"
-adorama="https://www.adorama.com/so3005718.html"
-# kohls
-# bnh
-
-if [[ $debug -eq "true" ]]
-then
-	# NOTE: these are links of currently purchaseable products
-	echo "DEBUG MODE"
-	bestbuy="https://www.bestbuy.com/site/apple-tv-4k-32gb-black/5901530.p"
-	target="https://www.target.com/p/apple-airpods-with-charging-case/-/A-54191097#lnk=sametab"
-	walmart="https://www.walmart.com/ip/Apple-AirPods-with-Charging-Case-Latest-Model/604342441"
-	newegg="https://www.newegg.com/white-phanteks-eclipse-p360a-atx-mid-tower/p/N82E16811854104"
-	adorama="https://www.adorama.com/ctc1000p1ssd.html"
-
-	# NOTE: XSX links to test on xsx launch day
-	bestbuy="https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p"
-	target="https://www.target.com/p/xbox-series-x-console/-/A-80790841"
-	walmart="https://www.walmart.com/ip/Xbox-Series-S/606518560"
-	# walmart="https://www.walmart.com/ip/Xbox-Series-X/443574645"
-	# NOTE: couldnt find a good xsx newegg link
-	newegg="https://www.newegg.com/p/N82E16868110292?Description=playstation%205&cm_re=playstation_5-_-68-110-292-_-Product"
-	adorama="https://www.adorama.com/xbrrt00001.html"
-fi
-
+index=0
 for store in $stores
 do
-	for index in $(seq 0 $num)
-	do
-		tag="$store.$index"
-		url_var=$store
-		url=${!url_var}
-		echo $tag
-		echo $url
+	tag="$store.$index"
+	url_var=$store
+	url=${!url_var}
+	echo $tag
+	echo $url
 
-		pm2 start bot.js --no-autorestart --name=$tag -- \
-			--store=$store --accountid=$index \
-			--url=$url \
-			--debug=$debug
-	done
+	pm2 start bot.js --no-autorestart --name=$tag -- \
+		--store=$store --accountid=$index \
+		--url=$url \
+		--debug=$debug
 done
 
 
