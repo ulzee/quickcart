accdf62057925b954907ed8057c9a6521d6fe24a
Ulzee An <ul1994@gmail.com>
2020-11-09 12:01:14 -0800

    more settings for bot dispatch

---
+++ batch.sh
@@ -0,0 +1,23 @@
+
+source ./urls.sh
+
+store=$1
+num=$2
+
+for index in $(seq 1 $num)
+do
+	tag="$store.$index"
+	url_var=$store
+	url=${!url_var}
+	echo ""
+	echo "Name : $tag"
+	echo "URL  : $url"
+	echo "Debug: $debug"
+
+	# pm2 start bot.js --no-autorestart --name=$tag -- \
+	# 	--store=$store --accountid=$index \
+	# 	--url=$url \
+	# 	--debug=$debug
+done
+
+
