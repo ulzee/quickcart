ef07ca2d65f1ef28204118ab6f0fa93c0e0ef911
Ulzee An <ul1994@gmail.com>
2020-11-11 15:56:34 -0800

    isolating just ps5 urls

--- urls.sh
+++ urls.sh
@@ -1,22 +1,6 @@
-
-# debug="true"
-
 # ps5
 bestbuy="https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p"
 target="https://www.target.com/p/playstation-5-console/-/A-81114595"
 walmart="https://www.walmart.com/ip/PlayStation-5-Console/363472942"
 newegg="https://www.newegg.com/p/N82E16868110292?Description=playstation%205&cm_re=playstation_5-_-68-110-292-_-Product"
 adorama="https://www.adorama.com/so3005718.html"
-# kohls
-# bnh
-
-if [[ $debug -eq "true" ]]
-then
-	# NOTE: these are links of currently purchaseable products
-	echo "DEBUG MODE"
-	bestbuy="https://www.bestbuy.com/site/apple-tv-4k-32gb-black/5901530.p"
-	target="https://www.target.com/p/apple-airpods-with-charging-case/-/A-54191097#lnk=sametab"
-	walmart="https://www.walmart.com/ip/Apple-AirPods-with-Charging-Case-Latest-Model/604342441"
-	newegg="https://www.newegg.com/white-phanteks-eclipse-p360a-atx-mid-tower/p/N82E16811854104"
-	adorama="https://www.adorama.com/ctc1000p1ssd.html"
-fi
