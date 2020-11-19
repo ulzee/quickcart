96e26bc95ad9d2134cb1e8b0d2b2c60b39e24904
Ulzee An <ul1994@gmail.com>
2020-11-18 22:03:15 -0800

    atc target using api

--- stores/target.js
+++ stores/target.js
@@ -1,7 +1,7 @@
 
 const sec = 1000;
 const utils = require('../utils');
-const { nav, any, inp, sel, paste, click, traffic } = require('../actions');
+const { nav, any, inp, sel, paste, atc, click, traffic } = require('../actions');
 
 const domain = 'https://www.target.com';
 
@@ -20,55 +20,71 @@ module.exports = {
 		yield nav.bench(page, args.url, waitFor='div[data-test="product-price"]');
 		yield page.waitForTimeout(sec);
 
+		function dismissPhone() {
+			page.waitForSelector('form[method="post"]', { timeout: 24 * 60 * 60 * sec })
+			.then(() => page.waitForTimeout(3*sec))
+			.then(() => page.$eval('form[method="post"]', el => el.nextSibling.click()))
+			.then(() => log('Dismissing Phone input...'))
+			.catch();
+		}
+		dismissPhone();
+
+		function dismissJoinCirlce() {
+			page.waitForSelector('#circle-skip', { timeout: 24 * 60 * 60 * sec })
+			.then(() => page.click('#circle-skip'))
+			.catch();
+		}
+		dismissJoinCirlce();
+
 		yield page.waitForSelector('#account');
 		yield click(page, '#account')
 		yield page.waitForTimeout(5 * sec);
 		yield click(page, '#accountNav-signIn');
 
 		yield page.waitForSelector('#username');
-		yield page.waitForTimeout(5 * sec);
+		yield page.waitForTimeout(2 * sec);
 		yield page.type('#username', user, { delay: 10 });
 		yield page.type('#password', pass, { delay: 10 });
 
 		yield page.waitForTimeout(sec);
 
 		yield page.click('#login');
-		yield page.waitForTimeout(5 * sec);
+		yield page.waitForTimeout(10 * sec);
 
 		yield nav.go(page, domain);
 		yield page.waitForTimeout(5 * sec);
 
-		// set Pickup store
-		const [accountIndex, batchID] = args.account.genid.split('_');
-		const zipCode = batchID;
+		// // set Pickup store
+		// const [accountIndex, batchID] = args.account.genid.split('_');
+		// const zipCode = batchID;
 
-		waitfor = traffic.match('v3/stores/nearby/');
-		yield click(page, '#storeId-utilityNavBtn');
-		yield waitfor;
-		yield page.waitForTimeout(sec);
+		// waitfor = traffic.match('v3/stores/nearby/');
+		// yield click(page, '#storeId-utilityNavBtn');
+		// yield waitfor;
+		// yield page.waitForTimeout(sec);
 
-		yield page.type('#zipOrCityState', zipCode);
-		yield page.waitForTimeout(sec);
-		waitfor = traffic.match('v3/stores/nearby/');
-		yield click(page, 'button[data-test="storeLocationSearch-button"]');
-		yield waitfor;
-		yield page.waitForTimeout(sec);
-		yield page.evaluate(async ({ index }) => {
-			return new Promise((yes) => {
-				const block = document.querySelectorAll('div[class*="StoreIdSearchBlock__Spacing"]')[index+1];
-				const setButton = block.querySelector('button[data-test="storeId-listItem-setStore"]');
-				const confirmButton = block.querySelector('button[data-test="storeId-listItem-confirmStore"]');
-				if (setButton) {
-					setButton.click();
-				}
-				else if (confirmButton) {
-					confirmButton.click();
-				}
-				yes();
-			});
-		}, { index: parseInt(accountIndex) });
-		log('Store selected');
-		yield page.waitForTimeout(3*sec);
+		// yield page.type('#zipOrCityState', zipCode);
+		// yield page.waitForTimeout(sec);
+		// waitfor = traffic.match('v3/stores/nearby/');
+		// yield click(page, 'button[data-test="storeLocationSearch-button"]');
+		// yield waitfor;
+		// yield page.waitForTimeout(sec);
+		// yield page.evaluate(async ({ index }) => {
+		// 	return new Promise((yes) => {
+		// 		const block = document.querySelectorAll('div[class*="StoreIdSearchBlock__Spacing"]')[index+1];
+		// 		const setButton = block.querySelector('button[data-test="storeId-listItem-setStore"]');
+		// 		const confirmButton = block.querySelector('button[data-test="storeId-listItem-confirmStore"]');
+		// 		if (setButton) {
+		// 			setButton.click();
+		// 		}
+		// 		else if (confirmButton) {
+		// 			confirmButton.click();
+		// 		}
+		// 		yes();
+		// 	});
+		// }, { index: parseInt(accountIndex) });
+		// log('Store selected');
+		// yield page.waitForTimeout(3*sec);
 
 		// empty cart
 		log('Emptying cart...');
@@ -80,7 +96,7 @@ module.exports = {
 		}
 		catch (e) {
 			while(true) {
-				const remaining = yield $$eval('button[data-test="cartItem-deleteBtn"]', ls => {
+				const remaining = yield page.$$eval('button[data-test="cartItem-deleteBtn"]', ls => {
 					if (!ls.length) return 0;
 
 					ls[0].click();
@@ -100,34 +116,77 @@ module.exports = {
 		waitfor = traffic.match('redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1');
 		yield nav.bench(page, url, waitFor='div[data-test="product-price"]');
 		yield waitfor;
+		yield page.waitForTimeout(5 * sec);
 	},
 	*standby(page, args) {
-		let loaded = false;
+		yield page.waitForSelector('div[data-test="product-price"]');
+
 		while(true) {
-			try {
-				yield page.waitForSelector('button[class*="styles__StyledButton"]', { timeout: 500 });
-				break;
-			}
-			catch(e) {
-				const outOfStockText = yield page.evaluate(() => {
-					const cantBuy = document.querySelector('div[data-test="storeBlockNonBuyableMessages"]');
-					if (cantBuy) {
-						return cantBuy.textContent;
-					}
-					return null;
-				});
-				if (outOfStockText) {
-					log('OOS: ' + outOfStockText);
+
+			const apiKey = yield page.evaluate(() => {
+				return window.__PRELOADED_STATE__.config.services.checkout.apiKey;
+			});
+			log('API Key: ' + apiKey);
+
+			const TCIN = yield page.evaluate(() => {
+				const tcinMapping = window.__PRELOADED_STATE__.selectedTcins;
+				const statedTCIN = Object.keys(tcinMapping)[0];
+				if (statedTCIN) {
+					const selectedTCIN = tcinMapping[statedTCIN];
+					return selectedTCIN;
 				}
+				else {
+					// Fallback based on browser
+					const parts = document.URL.split('-')
+					return parts[parts.length - 1];
+				}
+			});
+			log('TCIN: ' + TCIN);
 
-				const waitTime = utils.eta();
-				log('Waiting: ' + waitTime.toFixed(2));
-				yield page.waitForTimeout(waitTime * sec);
 
-				waitfor = traffic.match('redsky.target.com/redsky_aggregations/v1/web/pdp_fulfillment_v1');
-				yield nav.bench(page, args.url, waitFor='div[data-test="product-price"]', retry=true);
-				yield waitfor;
-			}
+			// NOTE: idk what channel_id does
+			const added = yield atc(page, {
+				url: `https://carts.target.com/web_checkouts/v1/cart_items?field_groups=CART%2CCART_ITEMS%2CSUMMARY&key=${apiKey}`,
+				params: {
+					headers: {
+						"User-Agent": args.userAgent,
+						"Accept": "application/json",
+						"Accept-Language": "en-US,en;q=0.5",
+						"Content-Type": "application/json",
+						"cache-control": "no-cache",
+						"pragma": "no-cache",
+						"sec-fetch-dest": "empty",
+						"sec-fetch-mode": "cors",
+						"sec-fetch-site": "same-site",
+						"x-application-name": "web",
+					},
+					referrer: args.url,
+					body: JSON.stringify({
+						cart_item: {
+							item_channel_id: "10",
+							quantity: 1,
+							tcin: TCIN,
+						},
+						cart_type: "REGULAR",
+						channel_id: "10",
+						fulfillment: {
+							fulfillment_test_mode: "grocery_opu_team_member_test"
+						},
+						shopping_context: "DIGITAL",
+					}),
+					method: 'POST',
+				}
+			}, res => {
+				return res.status < 300;
+			});
+
+			yield page.waitForTimeout(60 * sec);
+
+			if (added) break;
+
+			const waitTime = utils.eta();
+			log('Waiting: ' + waitTime.toFixed(2));
+			yield page.waitForTimeout(waitTime * sec);
 		}
 	},
 	*checkout(page, args) {
@@ -140,27 +199,29 @@ module.exports = {
 			logid,
 		} = args;
 
-		log(url);
+		function dismissSurvey() {
+			page.waitForSelector('#kplDeclineButton', { timeout: 24 * 60 * 60 * sec })
+			.then(() => page.click('#kplDeclineButton'))
+			.catch()
+		}
+		dismissSurvey();
 
-		//
-		// Choose the first available purchase option
+		// waitfor = traffic.match('carts.target.com/web_checkouts/v1/cart_items');
+		// yield page.$$eval('button[class*="styles__StyledButton"]', ls => {
+		// 	const validButtons = ls.filter(el =>
+		// 		el.attributes['data-test'].value != 'scheduledDeliveryButton');
 
-		waitfor = traffic.match('carts.target.com/web_checkouts/v1/cart_items');
-		yield page.$$eval('button[class*="styles__StyledButton"]', ls => {
-			const validButtons = ls.filter(el =>
-				el.attributes['data-test'].value != 'scheduledDeliveryButton');
+		// 	// favor delivery option button
+		// 	if (validButtons.length >= 2) {
+		// 		validButtons[1].click();
+		// 	}
+		// 	else {
+		// 		validButtons[0].click();
+		// 	}
+		// });
 
-			// favor delivery option button
-			if (validButtons.length >= 2) {
-				validButtons[1].click();
-			}
-			else {
-				validButtons[0].click();
-			}
-		});
-
-		// wait for confirmation prompt then go to cart
-		yield waitfor;
+		// // wait for confirmation prompt then go to cart
+		// yield waitfor;
 
 		// go straight to checkout
 		waitfor = traffic.match('carts.target.com/web_checkouts/v1/pre_checkout');
