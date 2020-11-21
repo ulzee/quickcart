64087ff7cbae7b1f3505d28d4f493241dea32669
Ulzee An <ul1994@gmail.com>
2020-11-21 00:04:31 -0800

    adding bjs url

--- urls.sh
+++ urls.sh
@@ -11,6 +11,7 @@ adorama_ps5="https://www.adorama.com/so3005718.html"
 walmart_debug="https://www.walmart.com/ip/Apple-AirPods-with-Charging-Case-Latest-Model/604342441"
 target_debug="https://www.target.com/p/marvel-39-s-spider-man-miles-morales-launch-edition-8211-playstation-5/-/A-81255580"
 bestbuy_debug="https://www.bestbuy.com/site/apple-airpods-with-charging-case-latest-model-white/6084400.p?skuId=6084400"
+bjs_debug="https://www.bjs.com/product/Clorox-Clean-Up-Cleaner-with-Bleach-Spray-Bottle-32-oz-with-Refill-Bottle-180-oz/3000000000000154389"
 
 #xsx
 bestbuy_xsx="https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p"
@@ -18,6 +19,7 @@ target_xsx="https://www.target.com/p/xbox-series-x-console/-/A-80790841"
 newegg_xsx="https://www.newegg.com/Product/ComboDealDetails?ItemList=Combo.4205033"
 # adorama="https://www.adorama.com/xbrrt00001.html"
 # bnh="https://www.bhphotovideo.com/c/product/1600080-REG/microsoft_rrt_00001_xbox_series_x_1tb.html"
+bjs_xsx="https://www.bjs.com/product/xbox-series-x-1tb-console/3000000000002702247"
 
 # NVIDIA
 bestbuy_3080fe="https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
