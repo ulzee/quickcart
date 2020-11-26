3be2cb85464b02b8328cacefe010f284adc0b799
Ulzee An <ul1994@gmail.com>
2020-11-26 10:05:45 -0800

    fixed typo in batch script

--- batch.sh
+++ batch.sh
@@ -4,6 +4,7 @@ source ./urls.sh
 store=$1
 item=$4
 sync=$5
+manual=$6
 
 for index in $(seq $2 $3)
 do
@@ -11,16 +12,18 @@ do
 	url_var="${store}_${item}"
 	url=${!url_var}
 	echo ""
-	echo "Name : $tag"
-	echo "URL  : $url"
-	echo "Debug: $debug"
-	echo "Sync : $sync"
+	echo "Name  : $tag"
+	echo "URL   : $url"
+	echo "Debug : $debug"
+	echo "Sync  : $sync"
+	echo "Manual: $manual"
 
 	pm2 start bot.js --no-autorestart --name=$tag -- \
 		--store=$store --accountid=$index \
 		--url=$url \
 		--item=$item \
-		--sync=$sync
+		--sync=$sync \
+		--manual=$manual
 done
 
 
